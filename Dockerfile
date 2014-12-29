fd865967bc53b5e8f528d01106e4dc887acbfed1
root <root@IL1CLABREGTES01.local>
2014-12-29 23:03:46 +0000

    Initial commit on ssl proxy via nginx

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+from nginx
+
+WORKDIR /usr/src
+
+ADD . /usr/src/
+
+RUN chmod +x run.sh
+
+CMD ./run.sh
---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cp /usr/src/docker-registry.conf /tmp/docker-registry.conf
+xyz=$(sed "s/<docker_registry_ip>/${REGISTRY_PORT_5000_TCP_ADDR}/g;s/<public_ip>/${PUBLIC_IP_ADDR}/g" /tmp/docker-registry.conf)
+echo "$xyz" > /etc/nginx/conf.d/docker-registry.conf
+echo "-----------------------"
+cat /etc/nginx/conf.d/docker-registry.conf
+echo "-----------------------"
+nginx -g 'daemon off;'
